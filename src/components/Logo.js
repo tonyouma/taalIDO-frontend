955a24efe6f644ca121e5ac37d1a0e4f39ae24b8
이준호 <helloljho@gmail.com>
2021-03-26 12:18:15 +0900

    다크모드 시 로고 색상 변경

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Box } from '@material-ui/core';
-
+import { useSelector } from 'react-redux';
 // ----------------------------------------------------------------------
 
 Logo.propTypes = {
@@ -9,13 +9,22 @@ Logo.propTypes = {
 };
 
 function Logo({ className, ...other }) {
+  const { auth, profile } = useSelector((state) => state.firebase);
+  const { themeMode } = useSelector((state) => state.settings);
+
   const length = new String(className).split(' ').length;
   let logoFile;
-  if (length > 1 || className === undefined) {
-    logoFile = '/static/brand/logo_b/logo_single.svg';
+
+  if (themeMode === 'light') {
+    if (length > 1 || className === undefined) {
+      logoFile = '/static/brand/logo_b/logo_single.svg';
+    } else {
+      logoFile = '/static/brand/logo_w/logo_single.svg';
+    }
   } else {
     logoFile = '/static/brand/logo_w/logo_single.svg';
   }
+
   return (
     <Box
       component="img"
