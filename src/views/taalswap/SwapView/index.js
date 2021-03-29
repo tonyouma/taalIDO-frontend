8e36d4e20671a4f5030055e5c9a11220f5fcbd59
이준호 <helloljho@gmail.com>
2021-03-29 18:51:53 +0900

    pool view 원복

--- src/views/taalswap/SwapView/index.js
+++ src/views/taalswap/SwapView/index.js
@@ -1,63 +1,281 @@
-import React from 'react';
-import Page from 'src/components/Page';
-import BasicTable from './BasicTable';
-import { PATH_APP } from 'src/routes/paths';
-import CollapsibleTable from './CollapsibleTable';
-import SortingSelecting from './SortingSelecting';
-import GroupingFixedHeader from './GroupingFixedHeader';
-import { HeaderDashboard } from 'src/layouts/Common';
+import React, { useState, useEffect, useCallback } from 'react';
+import { useLocation } from 'react-router';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Card, Container, CardHeader } from '@material-ui/core';
-
+import Page from '../../../components/Page';
+import {
+  Box,
+  Card,
+  CardHeader,
+  Container,
+  Grid,
+  Button,
+  Typography,
+  TextField,
+  LinearProgress
+} from '@material-ui/core';
+import { useContract } from '../../../hooks/useContract';
+import talkData from '../../../contracts/Talken.json';
+import fixedData from '../../../contracts/FixedSwap.json';
+import { useDispatch, useSelector } from 'react-redux';
+import getRatio from '../../../utils/getRatio';
+import getMax from '../../../utils/getMax';
+import getProgressValue from '../../../utils/getProgressValue';
 // ----------------------------------------------------------------------
 
 const useStyles = makeStyles((theme) => ({
-  root: {}
+  root: {},
+  boxButtons: {
+    marginTop: '1rem',
+    marginLeft: '1rem'
+  },
+  box: {
+    marginTop: '1rem',
+    marginBottom: '1rem',
+    paddingLeft: '1rem',
+    paddingRight: '1rem'
+  },
+  box2rem: {
+    marginTop: '2rem',
+    marginBottom: '2rem',
+    paddingLeft: '1rem',
+    paddingRight: '1rem'
+  }
 }));
 
-// ----------------------------------------------------------------------
-
-function TableView() {
+function SwapView() {
   const classes = useStyles();
+  const location = useLocation();
+
+  const [symbol, setSymbol] = useState('');
+  const [ratio, setRatio] = useState(0);
+  const [max, setMax] = useState(0);
+  const [allocated, setAllocated] = useState(0);
+  const [progressValue, setProgressValue] = useState(0);
+  const [participants, setParticipants] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isPreStart, setIsPreStart] = useState(false);
+  const [isSaleFunded, setIsSaleFunded] = useState(false);
+  const [bidAmount, setBidAmount] = useState(0);
+
+  const selectedPool = location.state.selectedPool;
+
+  const talkContract = useContract(selectedPool.address, talkData.abi);
+  const fixedContract = useContract(selectedPool.contract, fixedData.abi);
+
+  const onChangeBidAmount = (e) => {
+    e.preventDefault();
+    setBidAmount(e.target.value);
+  };
+
+  const onClickGo = () => {
+    console.log(bidAmount);
+  };
+
+  const fetchData = useCallback(async () => {
+    !!talkContract &&
+      (await talkContract.functions
+        .symbol()
+        .then((result) => setSymbol(result.tokenSymbol))
+        // .then((result) => console.log(result))
+        .catch((error) => console.log(error)));
+
+    !!fixedContract &&
+      (await fixedContract.functions
+        .tokensAllocated()
+        .then((result) => setAllocated(result))
+        .catch((error) => console.log(error)));
+
+    !!fixedContract &&
+      (await fixedContract.functions
+        .getBuyers()
+        .then((result) => {
+          const buyers = result[0].filter((buyer) => buyer !== null);
+          setParticipants(buyers.length);
+        })
+        .catch((error) => console.log(error)));
+
+    !!fixedContract &&
+      (await fixedContract.functions
+        .isOpen()
+        .then((result) => setIsOpen(result[0]))
+        .catch((error) => console.log(error)));
+
+    !!fixedContract &&
+      (await fixedContract.functions
+        .isPreStart()
+        .then((result) => setIsPreStart(result[0]))
+        .catch((error) => console.log(error)));
+
+    !!fixedContract &&
+      (await fixedContract.functions
+        .isSaleFunded()
+        .then((result) => setIsSaleFunded(result[0]))
+        .catch((error) => console.log(error)));
+  }, [talkContract]);
+
+  useEffect(() => {
+    console.log(selectedPool);
+    fetchData();
+
+    setRatio(getRatio(selectedPool.value));
+    setMax(getMax(selectedPool.max, selectedPool.value));
+    setProgressValue(getProgressValue(allocated, selectedPool.sale));
+  }, [fetchData]);
 
   return (
-    <Page title="Table-Components | Minimal-UI" className={classes.root}>
+    <Page title="Swap | IDO" className={classes.root}>
       <Container maxWidth="lg">
-        <HeaderDashboard
-          heading="Table"
-          links={[
-            { name: 'Dashboard', href: PATH_APP.root },
-            { name: 'Components', href: PATH_APP.components.root },
-            { name: 'Table' }
-          ]}
-          moreLink="https://next.material-ui.com/components/tables"
-        />
-
-        <Grid container spacing={5}>
+        <Grid container>
           <Grid item xs={12}>
             <Card>
-              <CardHeader title="Basic Table" />
-              <BasicTable />
-            </Card>
-          </Grid>
+              <CardHeader title={selectedPool.name} />
+              <Grid container>
+                <Grid item xs={12} sm={12} md={6} lg={6}>
+                  <Box
+                    className={classes.boxButtons}
+                    sx={{
+                      '& > :not(style)': {
+                        m: 1
+                      }
+                    }}
+                  >
+                    <Button variant="contained">participate</Button>
+                    <Button variant="contained">project info</Button>
+                    <Button variant="contained">project news</Button>
+                  </Box>
+                </Grid>
+              </Grid>
+              <Grid container>
+                <Grid item xs={12} sm={12} md={6} lg={6}>
+                  <Box className={classes.box2rem}>
+                    <Box className={classes.box2rem}>
+                      <Typography variant="body1">
+                        0 (추후 연동) live
+                      </Typography>
+                      <Typography variant="body1">
+                        Participant : Public (추후 연동)
+                      </Typography>
+                    </Box>
+                    <Box className={classes.box2rem}>
+                      <TextField
+                        label="Fixed Swap Ratio"
+                        variant="standard"
+                        InputLabelProps={{
+                          shrink: true
+                        }}
+                        fullWidth
+                        value={`${ratio} ETH = 1 ${symbol}`}
+                      />
+                    </Box>
+                    <Box
+                      className={classes.box2rem}
+                      display="flex"
+                      justifyContent="space-between"
+                    >
+                      <TextField
+                        label="Price, $"
+                        variant="standard"
+                        InputLabelProps={{
+                          shrink: true
+                        }}
+                        value="0.008881 (추후 연동)"
+                        style={{ width: '49%' }}
+                      />
+                      <TextField
+                        label="Maximum Allocation per Wallet"
+                        variant="standard"
+                        InputLabelProps={{
+                          shrink: true
+                        }}
+                        style={{ width: '49%' }}
+                        value={`${max} ETH`}
+                      />
+                    </Box>
+                    <Box className={classes.box2rem} textAlign="center">
+                      Auction progress : 0 BNB / 10 BNB (추후 연동)
+                      <LinearProgress
+                        variant="determinate"
+                        value={0}
+                        style={{ marginTop: '0.5rem' }}
+                      />
+                    </Box>
+                  </Box>
+                </Grid>
+                <Grid item xs={12} sm={12} md={6} lg={6}>
+                  <Box style={{ margin: '2rem' }}>
+                    <Box className={classes.box} textAlign="center">
+                      <Typography variant="h3">Join The Pool</Typography>
+                    </Box>
+                    <Box
+                      className={classes.box}
+                      textAlign="center"
+                      color="gray"
+                    >
+                      <Typography variant="body2">
+                        0d : 5h : 22m : 51s
+                      </Typography>
+                    </Box>
+                    <Box
+                      className={classes.box}
+                      textAlign="center"
+                      color="gray"
+                    >
+                      <div
+                        style={{
+                          height: '3px',
+                          maxWidth: '100%',
+                          backgroundColor: 'black'
+                        }}
+                      />
+                    </Box>
+                    <Box
+                      className={classes.box}
+                      textAlign="center"
+                      color="gray"
+                      display="flex"
+                      justifyContent="space-between"
+                    >
+                      <Typography variant="body2">Your Bid Ammount</Typography>
+                      <Typography variant="body2">
+                        Blance : 0 BNB (추후 연동)
+                      </Typography>
+                    </Box>
+                    <Box
+                      className={classes.box}
+                      textAlign="center"
+                      color="gray"
+                      display="flex"
+                      justifyContent="space-between"
+                    >
+                      <TextField
+                        label="Bid Ammount"
+                        value={bidAmount}
+                        variant="standard"
+                        fullWidth
+                        onChange={onChangeBidAmount}
+                      />
+                    </Box>
+                    <Box className={classes.box} textAlign="center">
+                      <Button
+                        style={{
+                          width: '100%',
+                          height: '3.5rem',
+                          marginTop: '2rem'
+                        }}
+                        variant="contained"
+                        onClick={onClickGo}
+                      >
+                        Go
+                      </Button>
 
-          <Grid item xs={12}>
-            <Card>
-              <SortingSelecting />
-            </Card>
-          </Grid>
-
-          <Grid item xs={12}>
-            <Card>
-              <CardHeader title="Grouping & FixedHeader" />
-              <GroupingFixedHeader />
-            </Card>
-          </Grid>
-
-          <Grid item xs={12}>
-            <Card>
-              <CardHeader title="Collapsible Table" />
-              <CollapsibleTable />
+                      <Typography variant="caption">
+                        Have problems with Joing? Click here to read
+                        instructions
+                      </Typography>
+                    </Box>
+                  </Box>
+                </Grid>
+              </Grid>
             </Card>
           </Grid>
         </Grid>
@@ -66,4 +284,4 @@ function TableView() {
   );
 }
 
-export default TableView;
+export default SwapView;
