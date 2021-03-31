de54c0ee86fb4eb38a0a1e2c3d2755ad02a3b2b4
이준호 <helloljho@gmail.com>
2021-03-31 15:45:32 +0900

    validation check

--- src/utils/getProgressValue.js
+++ src/utils/getProgressValue.js
@@ -1,3 +1,9 @@
 export default function getProgressValue(allocated, sale) {
-  return allocated / sale;
+  let result = 0;
+
+  if (allocated !== '' && sale !== '') {
+    result = allocated / sale;
+  }
+
+  return result;
 }
