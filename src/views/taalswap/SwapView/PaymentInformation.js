8027d8d53288762efebf6c09480456bfc5ad183f
Peter H. Nahm <hwnahm@gmail.com>
2021-04-02 11:37:42 +0900

    Pool 상태 확인 코드 추가

--- src/views/taalswap/SwapView/PaymentInformation.js
+++ src/views/taalswap/SwapView/PaymentInformation.js
@@ -13,7 +13,8 @@ import { tokenData } from '../../../contracts';
 import { useWeb3React } from '@web3-react/core';
 import Application from 'taalswap-js/src/models';
 import LinearProgress from '@material-ui/core/LinearProgress';
-import usePoolStatus from 'src/hooks/usePoolStatus';
+
+import { PoolStatus } from '../../../utils/poolStatus';
 
 // ----------------------------------------------------------------------
 
@@ -63,7 +64,6 @@ PaymentInformation.propTypes = {
 function PaymentInformation({ className, pool, index }) {
   const classes = useStyles();
   const context = useWeb3React();
-  const poolList = usePoolStatus(pool);
 
   const [progressValue, setProgressValue] = useState(0);
   const [participants, setParticipants] = useState(0);
@@ -79,8 +79,7 @@ function PaymentInformation({ className, pool, index }) {
     error
   } = context;
 
-  useEffect(() => {
-    console.log(poolList);
+  useEffect(async () => {
     if (!!library) {
       const fixedContract = new Contract(
         pool.contractAddress,
@@ -119,6 +118,69 @@ function PaymentInformation({ className, pool, index }) {
           setProgressValue(getProgressValue(result, pool.tradeAmount));
         })
         .catch((error) => {});
+
+      // Pool의 현재 상태 확인
+      const isPreStart = await swapContract.isPreStart().catch(() => {});
+      const isFunded = await swapContract.isFunded().catch(() => {});
+      const isOpen = await swapContract.isOpen().catch(() => {});
+      const hasStarted = await swapContract.hasStarted().catch(() => {});
+      const hasFinalized = await swapContract.hasFinalized().catch(() => {});
+      const hasMinimumRaise = await swapContract
+        .hasMinimumRaise()
+        .catch(() => {});
+      const minimumRaiseAchieved = await swapContract
+        .minimumRaiseAchieved()
+        .catch(() => {});
+      console.log('isOpen :', isPreStart);
+      console.log('isOpen :', isOpen);
+      console.log('hasStarted :', hasStarted);
+      console.log('hasFinalized :', hasFinalized);
+      console.log('hasMinimumRaise :', hasMinimumRaise);
+      console.log('minimumRaiseAchieved :', minimumRaiseAchieved);
+
+      let poolStatus;
+      if (isOpen) {
+        poolStatus = PoolStatus.LIVE;
+      } else {
+        if (isPreStart) {
+          if (isFunded) {
+            poolStatus = PoolStatus.UPCOMING;
+          }
+        } else {
+          if (hasMinimumRaise) {
+            if (minimumRaiseAchieved) {
+              // 구매자
+              // getMyPurchases(지갑주소)
+              // -> return uint256[]
+              //    forEach :
+              //      redeemTokens(uint256 purchase_id)
+              // 판매자
+              // withdrawFunds()
+              poolStatus = PoolStatus.FILLED.SUCCESS.ACHIEVED;
+            } else {
+              // 구매자
+              // getMyPurchases(지갑주소)
+              // -> return uint256[]
+              //    forEach :
+              //      redeemGivenMinimumGoalNotAchieved(uint256 purchase_id)
+              // 판매자
+              // withdrawUnsoldTokens()
+              poolStatus = PoolStatus.FILLED.FAILED;
+            }
+          } else {
+            poolStatus = PoolStatus.FILLED.SUCCESS.CLOSED;
+            // 구매자
+            // getMyPurchases(지갑주소)
+            // -> return uint256[]
+            //    forEach :
+            //      redeemTokens(uint256 purchase_id)
+            // 판매자
+            // withdrawFunds()
+            // withdrawUnsoldTokens()
+          }
+        }
+      }
+      console.log('Pool Status :', poolStatus);
     }
   }, [pool]);
 
@@ -133,7 +195,7 @@ function PaymentInformation({ className, pool, index }) {
       {pool.status === 'deployed' ? (
         <MLabel color="success">Deployed</MLabel>
       ) : null}
-      {/*       
+      {/*
       <MLabel color="warning">Waring</MLabel>
       <MLabel color="error">Error</MLabel> */}
 
