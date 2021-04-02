6b2f6f980166a824176de3152cb1e25c7db1b47e
이준호 <helloljho@gmail.com>
2021-04-02 14:29:33 +0900

    Participants 버튼명 수정, 나머지 버튼 제거

--- src/views/taalswap/SwapView/PoolButton.js
+++ src/views/taalswap/SwapView/PoolButton.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
+import { Link as RouterLink } from 'react-router-dom';
+import { PATH_APP } from 'src/routes/paths';
 
 export default function ContainedButtons() {
   return (
@@ -9,15 +11,20 @@ export default function ContainedButtons() {
         '& > :not(style)': { m: 1 }
       }}
     >
-      <Button color="error" variant="contained">
-        Participate
+      <Button
+        to={PATH_APP.taalswap.participate}
+        component={RouterLink}
+        color="error"
+        variant="contained"
+      >
+        Participants
       </Button>
-      <Button color="info" variant="contained">
+      {/* <Button color="info" variant="contained">
         Project Info
       </Button>
       <Button color="info" variant="contained">
         Project News
-      </Button>
+      </Button> */}
     </Box>
   );
 }
