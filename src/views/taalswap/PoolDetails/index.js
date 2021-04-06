9f86a6150adb570ed66ee6d9121e83eb56e6d914
이준호 <helloljho@gmail.com>
2021-04-06 13:23:33 +0900

    swap 뷰 탭컴포넌트 추가

--- src/views/taalswap/PoolDetails/index.js
+++ src/views/taalswap/PoolDetails/index.js
@@ -7,35 +7,10 @@ import BasicTable from './BasicTable';
 import roundAccountBox from '@iconify-icons/ic/round-account-box';
 import { HeaderDashboard } from 'src/layouts/Common';
 import { makeStyles } from '@material-ui/core/styles';
-import {
-  Grid,
-  Card,
-  Container,
-  CardHeader,
-  CardContent,
-  Tabs,
-  Tab
-} from '@material-ui/core';
+import { Container } from '@material-ui/core';
 
 // ----------------------------------------------------------------------
 
-const ACCOUNT_TABS = [
-  {
-    value: 'Pool Details',
-    icon: <Icon icon={roundAccountBox} width={20} height={20} />,
-    component: <BasicTable />
-  },
-  {
-    value: 'About the Project',
-    icon: <Icon icon={roundAccountBox} width={20} height={20} />,
-    component: <index2 />
-  },
-  {
-    value: 'Your Allocations',
-    icon: <Icon icon={roundAccountBox} width={20} height={20} />,
-    component: <index2 />
-  }
-];
 const useStyles = makeStyles((theme) => ({
   root: { marginBottom: theme.spacing(5) },
   tabBar: {
@@ -54,29 +29,6 @@ function PoolDetails({ pool }) {
   return (
     <Page title="My Pools | TaalSwap" className={classes.root}>
       <Container maxWidth="lg">
-        <HeaderDashboard
-          heading="Pool Details"
-          links={[{ name: 'SubText Display Area' }]}
-        />
-
-        <Tabs
-          value={currentTab}
-          scrollButtons="auto"
-          variant="scrollable"
-          allowScrollButtonsMobile
-          onChange={handleChangeTab}
-          className={classes.tabBar}
-        >
-          {ACCOUNT_TABS.map((tab) => (
-            <Tab
-              disableRipple
-              key={tab.value}
-              label={capitalCase(tab.value)}
-              icon={tab.icon}
-              value={tab.value}
-            />
-          ))}
-        </Tabs>
         <BasicTable pool={pool} />
       </Container>
     </Page>
