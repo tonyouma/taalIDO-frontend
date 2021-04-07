9666f8b1006f4f7a32133a1a1b7a7474853d72bc
seongjin1026 <seongjin.kim@owdin.network>
2021-04-07 20:40:06 +0900

    status ready 설정 변경

--- src/views/taalswap/Components/StatusLabel.js
+++ src/views/taalswap/Components/StatusLabel.js
@@ -60,7 +60,7 @@ const StatusLabel = ({ poolStatus, absolute }) => {
             right: 16,
             position: absolute && 'absolute'
           }}
-          color="failed"
+          color="default"
         >
           Ready
         </MLabel>
