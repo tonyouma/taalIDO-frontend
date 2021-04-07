97e7ccf205283be24273fb27e8a26a589e44f36b
이준호 <helloljho@gmail.com>
2021-04-07 15:12:59 +0900

    Application List pagination 추가

--- src/views/taalswap/ApplicationView/List/index.js
+++ src/views/taalswap/ApplicationView/List/index.js
@@ -615,51 +615,56 @@ export default function ApplicationListView() {
                     {stableSort(
                       filteredApplications,
                       getComparator(order, orderBy)
-                    ).map((row, index) => {
-                      const isItemSelected = isSelected(row.id);
-                      const labelId = `enhanced-table-checkbox-${index}`;
-                      return (
-                        <TableRow
-                          hover
-                          onClick={(event) => handleClick(event, row)}
-                          role="checkbox"
-                          aria-checked={isItemSelected}
-                          tabIndex={-1}
-                          key={row.id}
-                          selected={isItemSelected}
-                        >
-                          <TableCell padding="checkbox">
-                            <Checkbox
-                              checked={isItemSelected}
-                              inputProps={{ 'aria-labelledby': labelId }}
-                            />
-                          </TableCell>
-                          <TableCell
-                            component="th"
-                            id={labelId}
-                            scope="row"
-                            padding="none"
+                    )
+                      .slice(
+                        page * rowsPerPage,
+                        page * rowsPerPage + rowsPerPage
+                      )
+                      .map((row, index) => {
+                        const isItemSelected = isSelected(row.id);
+                        const labelId = `enhanced-table-checkbox-${index}`;
+                        return (
+                          <TableRow
+                            hover
+                            onClick={(event) => handleClick(event, row)}
+                            role="checkbox"
+                            aria-checked={isItemSelected}
+                            tabIndex={-1}
+                            key={row.id}
+                            selected={isItemSelected}
                           >
-                            {row.projectName}
-                          </TableCell>
-                          <TableCell align="right">{row.category}</TableCell>
-                          <TableCell align="right">
-                            {moment.unix(row.startDate).format('YYYY-MM-DD')}
-                          </TableCell>
-                          <TableCell align="right">
-                            {moment.unix(row.endDate).format('YYYY-MM-DD')}
-                          </TableCell>
-                          <TableCell align="right">{row.status}</TableCell>
-                        </TableRow>
-                      );
-                    })}
+                            <TableCell padding="checkbox">
+                              <Checkbox
+                                checked={isItemSelected}
+                                inputProps={{ 'aria-labelledby': labelId }}
+                              />
+                            </TableCell>
+                            <TableCell
+                              component="th"
+                              id={labelId}
+                              scope="row"
+                              padding="none"
+                            >
+                              {row.projectName}
+                            </TableCell>
+                            <TableCell align="right">{row.category}</TableCell>
+                            <TableCell align="right">
+                              {moment.unix(row.startDate).format('YYYY-MM-DD')}
+                            </TableCell>
+                            <TableCell align="right">
+                              {moment.unix(row.endDate).format('YYYY-MM-DD')}
+                            </TableCell>
+                            <TableCell align="right">{row.status}</TableCell>
+                          </TableRow>
+                        );
+                      })}
                   </TableBody>
                 </Table>
               </TableContainer>
               <TablePagination
                 page={page}
                 component="div"
-                count={BasicTable.length}
+                count={filteredApplications.length}
                 rowsPerPage={rowsPerPage}
                 onPageChange={handleChangePage}
                 rowsPerPageOptions={[10, 25, 100]}
