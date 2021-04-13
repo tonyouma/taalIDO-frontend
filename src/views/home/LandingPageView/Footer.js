99dbe36897dd66dfb929c0408cbb28293cb170ad
hanabpg <hana.kim@di-communications.com>
2021-04-13 21:01:30 +0900

    footer 정렬 및 간소화

--- src/views/home/LandingPageView/Footer.js
+++ src/views/home/LandingPageView/Footer.js
@@ -1,52 +1,83 @@
 import clsx from 'clsx';
 import React from 'react';
 import PropTypes from 'prop-types';
+//import Block from 'src/components/Block';
+import { motion } from 'framer-motion';
 import Logo from 'src/components/Logo';
 import { makeStyles } from '@material-ui/core/styles';
-import { motion } from 'framer-motion';
-import { Link, Container, Typography } from '@material-ui/core';
+import { Container, Typography } from '@material-ui/core';
+
 // ----------------------------------------------------------------------
 
 const useStyles = makeStyles((theme) => ({
+  // page 3-2 padding 변경
   root: {
-    width: '100%',
-    height: '100%',
     textAlign: 'center',
-    padding: theme.spacing(5, 0),
+    paddingTop: 50,
+    paddingBottom: 20,
     backgroundColor: theme.palette.grey[900]
+  },
+  content: {
+    textAlign: 'center',
+    position: 'absolute',
+    marginBottom: theme.spacing(100),
+    [theme.breakpoints.up('md')]: {
+      height: '100%',
+      marginBottom: '100',
+      textAlign: 'left',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'flex-start'
+    },
+    [theme.breakpoints.down('md')]: {
+      height: '100%',
+      width: '100%',
+      marginBottom: '100',
+      textAlign: 'left',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }
   },
   listIcon: {
     display: 'flex',
+    marginBottom: theme.spacing(5),
     marginTop: theme.spacing(3),
     justifyContent: 'center',
-    marginRight: theme.spacing(4.5)
-  }
+    [theme.breakpoints.up('md')]: {},
+    '& > :not(:last-of-type)': {
+      marginRight: theme.spacing(4.5)
+    }
+  },
+  handleOn: { width: 20 }
 }));
 
 // ----------------------------------------------------------------------
 
-Footer.propTypes = {
+DarkMode.propTypes = {
   className: PropTypes.string
 };
 
-function Footer({ className }) {
+function DarkMode({ className }) {
   const classes = useStyles();
 
   return (
-    <Container maxWidth="lg" className={clsx(classes.root, className)}>
-      <Logo sx={{ mb: 1, mx: 'auto' }} />
+    <div className={clsx(classes.root, className)}>
+      <Container maxWidth="lg" sx={{ position: 'relative' }}>
+        <Logo sx={{ mb: 1, mx: 'auto' }} />
 
-      <Typography color="common.white" variant="caption">
-        © All rights reserved. Made by TaalSwap.
-      </Typography>
-      <div className={classes.listIcon}>
-        <motion.img src="/static/icons/ic_s_facebook.svg" />
-        <motion.img src="/static/icons/ic_s_linkin.svg" />
-        <motion.img src="/static/icons/ic_s_instagram.svg" />
-        <motion.img src="/static/icons/ic_s_tweet.svg" />
-      </div>
-    </Container>
+        <Typography color="common.white" variant="caption">
+          © All rights reserved. Made by TaalSwap.
+        </Typography>
+        <div className={classes.listIcon}>
+          <motion.img src="/static/icons/ic_s_facebook.svg" />
+          <motion.img src="/static/icons/ic_s_linkin.svg" />
+          <motion.img src="/static/icons/ic_s_instagram.svg" />
+          <motion.img src="/static/icons/ic_s_tweet.svg" />
+        </div>
+      </Container>
+    </div>
   );
 }
 
-export default Footer;
+export default DarkMode;
