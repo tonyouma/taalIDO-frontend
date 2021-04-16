08c1adf50fa868c543ef1b8717b6776fd3399a57
seongjin1026 <seongjin.kim@owdin.network>
2021-04-16 19:48:03 +0900

    imageUrl 추가 작업 중

--- src/views/taalswap/ApplicationView/Start/index.js
+++ src/views/taalswap/ApplicationView/Start/index.js
@@ -46,7 +46,8 @@ function ApplicationStart() {
   const NewApplicationSchema = Yup.object().shape({
     name: Yup.string().required('Project Name is required'),
     category: Yup.string().required('Category is required'),
-    websiteUrl: Yup.string().required('Website URL is required'),
+    websiteUrl: Yup.string().url().required('Website URL is required'),
+    imageUrl: Yup.string().url().required('ICON image URL is required'),
     email: Yup.string().email().required('eMail is required'),
     telegramHandle: Yup.string().required('Telegram handle is required'),
     poolName: Yup.string().required('Pool Name is required'),
@@ -78,6 +79,7 @@ function ApplicationStart() {
         ? location.state.selectedItem.projectDesc
         : '',
       websiteUrl: location.state ? location.state.selectedItem.websiteUrl : '',
+      imageUrl: location.state ? location.state.selectedItem.imageUrl : '',
       email: location.state ? location.state.selectedItem.email : '',
       telegramHandle: location.state
         ? location.state.selectedItem.telegramHandle
@@ -118,6 +120,7 @@ function ApplicationStart() {
           category: values.category,
           projectDesc: values.projectDesc,
           websiteUrl: values.websiteUrl,
+          imageUrl: values.imageUrl,
           email: values.email,
           telegramHandle: values.telegramHandle,
           poolName: values.poolName,
