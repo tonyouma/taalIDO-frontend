dedc64913b44d97103ceb8b8e63ce97f8a854492
seongjin1026 <seongjin.kim@owdin.network>
2021-04-19 11:50:04 +0900

    config.js 의 설정을 .env 를 사용하도록 수정

--- src/config.js
+++ src/config.js
@@ -20,24 +20,16 @@ export const cloudinaryConfig = {
   cloudinaryUrl: process.env.REACT_APP_CLOUDINARY_URL
 };
 
-export const infuraApiKey = 'fbb83d21738f48d7bccfc214aa014f75';
-export const infuraChainId = 'rinkeby';
+export const infuraApiKey = process.env.REACT_APP_INFURA_API_KEY;
+export const infuraChainId = process.env.REACT_APP_INFURA_CHAIN_ID;
 
-export const targetNetwork = '0x4';
-export const targetNetworkMsg =
-  'Rinkeby 테스트 네트워크가 선택되지 않았습니다.';
+export const targetNetwork = process.env.REACT_APP_TARGET_NETWORK;
+export const targetNetworkMsg = process.env.REACT_APP_TARGET_NETWORK_MSG;
 
-export const jsonServerUrl = 'http://taalswap.finance:3002';
+export const jsonServerUrl = process.env.REACT_APP_JSON_SERVER_URL;
 
 export const admin = {
-  addresses: [
-    '0x3AeFa0a9222D69B535EA0A2fAE25768De8c9BBd1',
-    '0x623C74254371730Ec947CBFb706659F0675A5ff6',
-    '0xa42f6EE885e29a6915269B34e249f2a1c0A26955',
-    '0x8f6EE14BaDa5c01f4717503EC7102A77d5d58026',
-    '0x19922f6D6D3a203cBa4dD6fEBefe18BDc036C674',
-    '0xD77f21211405a10f169d48ac729f70Edfcb41075'
-  ]
+  addresses: process.env.REACT_APP_ADMIN_ADDRESSES
 };
 
 export const googleAnalyticsConfig = process.env.REACT_APP_GA_MEASUREMENT_ID;
